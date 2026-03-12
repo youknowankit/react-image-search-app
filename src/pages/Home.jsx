b7c5fb95eacd897a87ca12d91ebd0e22bc4eb9da
youknowankit <thisisankityadav@gmail.com>
2026-03-12 19:05:45 +0530

    Add infinite scrolling to the home page and wrote a messy code| will get fixed to custom hook next update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,15 +7,25 @@ import { searchImagesApi } from "../services/imageApi";
 function Home() {
   const [images, setImages] = useState([]);
 
-  const handleSearch = async (query) => {
+  //Infinite Scroll additions
+  const [query, setQuery] = useState("nature");
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  const handleSearch = async (searchQuery) => {
     try {
-      const images = await searchImagesApi(query);
-      setImages(images);
+      //Preserve data for infinite scroll
+      setQuery(searchQuery);
+      setPage(1);
+
+      const results = await searchImagesApi(searchQuery, 1);
+      setImages(results);
     } catch (error) {
       console.log(error.message);
     }
   };
 
+  //To fetch default images
   useEffect(() => {
     async function loadImages() {
       const images = await searchImagesApi("nature");
@@ -23,6 +33,34 @@ function Home() {
     }
     loadImages();
   }, []);
+
+  useEffect(() => {
+    const loadMore = async () => {
+      //useRef could do better here as the updates are synchronous rather than being async
+      if (loading) return;
+      setLoading(true);
+
+      const nextPage = page + 1;
+      const results = await searchImagesApi(query, nextPage);
+
+      setImages((prev) => [...prev, ...results]);
+      setPage(nextPage);
+
+      setLoading(false);
+    };
+
+    const handleScroll = () => {
+      const bottom =
+        window.innerHeight + window.scrollY >=
+        document.documentElement.scrollHeight - 200;
+
+      if (bottom) {
+        loadMore();
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [query, page, loading]);
 
   return (
     <>
